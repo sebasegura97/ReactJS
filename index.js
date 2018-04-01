96f0e5fce74c6c1667780b8511144085c388988a
sebasegura97 <sebasegura97@gmail.com>
2018-04-01 01:44:17 -0300

    Estoy harto

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+
+console.log('hola mundoo');
+
+const app = getElementById('app')
+const holaMundo = <h1>Hola mundo</h1>
+// ReactDOM.render(que renderizo, donde lo renderizo)
+ReactDOM.render(holaMundo, app)
